723062b59638b720f9aeb25914afff9d033fe3f4
Glecinilson Silva <glecinilson@gmail.com>
2022-07-20 10:23:10 -0400

    Desafio 1 - Introdução ao SOLID

--- src/modules/users/repositories/implementations/UsersRepository.ts
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -20,22 +20,48 @@ class UsersRepository implements IUsersRepository {
 
   create({ name, email }: ICreateUserDTO): User {
     // Complete aqui
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    this.users.push(user);
+
+    return user;
   }
 
   findById(id: string): User | undefined {
     // Complete aqui
+    const user = this.users.find((user) => user.id === id);
+    return user;
   }
 
   findByEmail(email: string): User | undefined {
     // Complete aqui
+    const user = this.users.find((user) => user.email === email);
+    return user;
   }
 
   turnAdmin(receivedUser: User): User {
     // Complete aqui
+    const user = receivedUser;
+
+    user.name = receivedUser.name;
+    user.email = receivedUser.email;
+    user.admin = true;
+    user.created_at = new Date();
+    user.updated_at = new Date();
+
+    return user;
   }
 
   list(): User[] {
     // Complete aqui
+    return this.users;
   }
 }
 
